04d4188b148bef57438c1e10ff01245c005543b8
AmeerSafadi <ameersafadi1@gmail.com>
2020-07-06 16:35:09 +0300

    Updates

---
+++ precache-manifest.8379308c480b1b05d23dacea1e938495.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf05d5aa09394e087354eddf19ed853",
+    "url": "/Amir-Safadi-06-07-2020/index.html"
+  },
+  {
+    "revision": "bd95d066a81a7b5c8ae2",
+    "url": "/Amir-Safadi-06-07-2020/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "fa4dc035d84fc906f127",
+    "url": "/Amir-Safadi-06-07-2020/static/css/main.f0220437.chunk.css"
+  },
+  {
+    "revision": "bd95d066a81a7b5c8ae2",
+    "url": "/Amir-Safadi-06-07-2020/static/js/2.b292e063.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Amir-Safadi-06-07-2020/static/js/2.b292e063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4dc035d84fc906f127",
+    "url": "/Amir-Safadi-06-07-2020/static/js/main.2f34f70c.chunk.js"
+  },
+  {
+    "revision": "813a4a4e70aaacb44ce9",
+    "url": "/Amir-Safadi-06-07-2020/static/js/runtime-main.789614a1.js"
+  }
+]);
